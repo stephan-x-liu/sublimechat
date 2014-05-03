534a6ef053b882c60f8f2d6eb71f5e47dc717d43
Gautham Kesineni <gautham.kesineni.me@gmail.com>
2014-05-02 22:46:10 -0700

    ran some sample code

--- sample.py
+++ sample.py
@@ -2,4 +2,20 @@ import sublime, sublime_plugin
 
 class ExampleCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, "string")
+		print(type(edit))
+		print(type(self.view))
+		print(len(sublime.windows()))
+		for window in sublime.windows():
+			print([view.id() for view in window.views()])
+		window.show_input_panel("string", "text", func, None, None)
+		#sublime.active_window().new_file()
+		viewlist = sublime.active_window().views()
+		print(viewlist)
+		self.view.insert(edit, 0, "")
+		self.view.settings().set("name", "thing")
+		print(self.view.settings().get("name"))
+		print(self.view.settings().has("name"))
+
+def func(x):
+	print(x)
+	return x
